9735e89146d11483dd3f02cc4e44b9a2e708b069
Zach Sents <zachsents@gmail.com>
2022-07-11 02:14:09 -0600

    Initial commit

---
+++ Orchestrator/index.js
@@ -0,0 +1,20 @@
+﻿const df = require("durable-functions")
+
+
+module.exports = df.orchestrator(function* (context) {
+
+    const req = context.df.getInput()
+
+    // hit API running on Container Apps
+    const response = yield context.df.callActivity("RunOp", {
+        operation: req.params.opName,
+        data: req.body.data,
+        contentTypeHeader: req.headers['content-type']
+    })
+
+    // check for errors in the fetch
+    if (response.status >= 400)
+        return { error: `Received bad status from iBioSim API: ${response.status}` }
+    
+    return response.data
+})
---
+++ RunOp/index.js
@@ -0,0 +1,31 @@
+﻿
+module.exports = async function (context) {
+
+    // dynamic import b/c node-fetch is ESM only
+    const { default: fetch } = await import("node-fetch")
+
+    // grab our input data
+    const { operation, data, contentTypeHeader } = context.bindings.input
+
+    // execute request
+    const response = await fetch(
+        `https://ibiosim-api.wittyfield-d51873c4.westus.azurecontainerapps.io/sync/${operation}`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': contentTypeHeader
+            },
+            body: Buffer.from(data)
+        }
+    )
+
+    // Convert to a regular array of bytes so DF can handle it
+    const responseData = Array.from(
+        new Uint8Array(await response.arrayBuffer())
+    )
+
+    return {
+        data: responseData,
+        status: response.status
+    }
+}
