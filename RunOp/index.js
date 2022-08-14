8db6dc4d4a9d17110c23deeee770a2e52922799c
Zach Sents <zachsents@gmail.com>
2022-08-14 14:43:05 -0600

    Updated endpoint

--- RunOp/index.js
+++ RunOp/index.js
@@ -10,7 +10,7 @@ module.exports = async function (context) {
 
     // execute request
     const response = await fetch(
-        `https://ibiosim-api.wittyfield-d51873c4.westus.azurecontainerapps.io/sync/${operation}`,
+        `https://ibiosim-api.yellowsea-87128a49.westus.azurecontainerapps.io/sync/${operation}`,
         {
             method: 'POST',
             headers: {
