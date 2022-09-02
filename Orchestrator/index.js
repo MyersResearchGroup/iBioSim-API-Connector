c840f7ea00302375135dcf38b4efb921a944b32b
Zach Sents <zachsents@gmail.com>
2022-09-01 20:58:18 -0600

    Converted to using async endpoints. This allows jobs >10 mins.

--- Orchestrator/index.js
+++ Orchestrator/index.js
@@ -14,7 +14,24 @@ module.exports = df.orchestrator(function* (context) {
 
     // check for errors in the fetch
     if (response.status >= 400)
-        return { error: `Received bad status from iBioSim API: ${response.status}` }
-    
-    return response.data
+        throw new Error(`Received bad status from iBioSim API: ${response.status}`)
+
+    // wait for either a "complete" or "error" event
+    const completeEvent = context.df.waitForExternalEvent("complete")
+    const errorEvent = context.df.waitForExternalEvent("error")
+    const winner = yield context.df.Task.any([completeEvent, errorEvent])
+
+    // if we got "complete"
+    if(winner === completeEvent) {
+        // call decompressing activity
+        const { data: decompressed } = yield context.df.callActivity("Decompressor", {
+            data: winner.result.data,
+        })
+
+        return decompressed
+    }
+
+    // otherwise received error
+    console.error(winner.result)
+    throw Error("Received error event.")
 })
--- RunOp/index.js
+++ RunOp/index.js
@@ -1,4 +1,6 @@
-﻿const unzipper = require('unzipper')
+﻿
+const ibiosimFallback = "http://localhost:4000"
+const applicationUrlFallback = "http://host.docker.internal:7071"
 
 module.exports = async function (context) {
 
@@ -8,37 +10,41 @@ module.exports = async function (context) {
     // grab our input data
     const { operation, data, contentTypeHeader } = context.bindings.input
 
+    // attach callback to request
+    const callbackUrl = `${process.env.APPLICATION_URL || applicationUrlFallback}/runtime/webhooks/durabletask/instances/${context.bindingData.instanceId}/raiseEvent/{event}`
+    const fixedData = addToFormData(data, {
+        callback: callbackUrl,
+        json: true,
+    })
+
     // execute request
     const response = await fetch(
-        `${process.env.IBIOSIM_API}/sync/${operation}`,
+        `${process.env.IBIOSIM_API || ibiosimFallback}/async/${operation}`,
         {
             method: 'POST',
             headers: {
                 'Content-Type': contentTypeHeader
             },
-            body: Buffer.from(data)
+            body: Buffer.from(fixedData)
         }
     )
 
-    // Unzip response and send contents as strings
-    const responseData = {}
-    const zip = response.body.pipe(unzipper.Parse({ forceStream: true }))
-    for await (const entry of zip) {
-        responseData[entry.path] = await streamToString(entry)
-    }
-
     return {
-        data: responseData,
         status: response.status
     }
 }
 
 
-function streamToString(stream) {
-    const chunks = []
-    return new Promise((resolve, reject) => {
-        stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)))
-        stream.on('error', (err) => reject(err))
-        stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')))
-    })
+function addToFormData(data, pairsToAdd) {
+
+    const asString = Buffer.from(data).toString()
+    const boundary = asString.match(/-{10,}[\S]+/)?.[0]
+
+    return Object.entries(pairsToAdd).reduce((accum, [key, value]) =>
+        `${boundary}\r
+Content-Disposition: form-data; name="${key}"\r
+\r
+${value}\r
+${accum}`
+        , asString)
 }
